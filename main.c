cecaba078f57943262ca55e8075500e55b64a96b
burro21 <fifi9@sapo.pt>
2021-05-07 17:57:43 +0100

    terseiro

--- main.c
+++ main.c
@@ -26,7 +26,7 @@
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"                // SYS function prototypes
-
+int inicio;
 
 // *****************************************************************************
 // *****************************************************************************
